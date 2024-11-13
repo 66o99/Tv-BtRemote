4afea020543d0b58b1056e5ae0cbf4aedce65d89
Atharok <atharok@duck.com>
2024-11-13 12:06:26 +0100

    Updated dependencies and AGP.

--- app/src/main/java/com/atharok/btremote/common/injections/Modules.kt
+++ app/src/main/java/com/atharok/btremote/common/injections/Modules.kt
@@ -56,8 +56,8 @@ import com.atharok.btremote.presentation.viewmodel.BluetoothViewModel
 import com.atharok.btremote.presentation.viewmodel.GyroscopeSensorViewModel
 import com.atharok.btremote.presentation.viewmodel.SettingsViewModel
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModules by lazy {
