5b40455b4fdc440857bd3e4a453daebdc38458c4
66o99 <116540988+66o99@users.noreply.github.com>
2026-02-04 13:08:39 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,10 +54,10 @@ android {
      
             isShrinkResources = false  
             isMinifyEnabled = false // 暂时改为 false 防止闪退！
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+     //       proguardFiles(
+     //           getDefaultProguardFile("proguard-android-optimize.txt"),
+     //           "proguard-rules.pro"
+     //       )
         }
     }
 
